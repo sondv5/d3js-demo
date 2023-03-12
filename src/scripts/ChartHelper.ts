8902061bd0ef79d04a02690d6e0141b703ab184d
Dong Van Son <dvs96.ptit@gmail.com>
2023-03-12 21:55:16 +0700

    add stack area chart

--- src/scripts/ChartHelper.ts
+++ src/scripts/ChartHelper.ts
@@ -13,18 +13,18 @@ export abstract class Chart implements IChart {
         '#2C9D8B',
         '#264557'
     ];
-    
+
     static LEGEND_COLOR = '#808080';
-    
+
     static RANDOM_TIME = 3000;
     static PAUSE_TIME = 1000;
 
     svg: null | Selection<SVGSVGElement, unknown, HTMLElement, any> = null;
     create(selector: string): void {
-        throw new Error('Method not implemented.');
+        // console.warn('Method not implemented.');
     }
     refresh(): void {
-        throw new Error('Method not implemented.');
+        // console.warn('Method not implemented.');
     }
 }
 
@@ -74,39 +74,7 @@ export class BarChart extends Chart {
     }
 }
 
-export class ArrowBarChart extends BarChart {
-    bars: Selection<SVGRectElement, unknown, HTMLElement, undefined>[] = [];
-    arrow: null | Selection<SVGGElement, unknown, HTMLElement, any> = null;
-    create(selector: string): void {
-        super.create(selector);
-        this.arrow = this.svg?.append('g') || null;
-        this.arrow?.append('circle')
-            .attr('cx', 40)
-            .attr('cy', 90)
-            .attr('r', 3)
-            .style('fill', Chart.LEGEND_COLOR);
-        this.arrow?.append('path')
-            .attr('d', 'M40 90 L210 90 L210 87 L213 90 L210 93 L210 90 Z')
-            .style('fill', Chart.LEGEND_COLOR)
-            .style('stroke', Chart.LEGEND_COLOR)
-            .style('stroke-width', 2);
-        this.arrow?.style('transform-origin', 'center')
-            .style('transform', `translateY(30px) rotate(${Math.random() * 60 - 30}deg)`);
-    }
-    refresh(): void {
-        super.refresh();
-        this.bars.forEach((bar, i) => {
-            const height = Math.random() * 140 + 20;
-            bar.transition()
-                .duration(Chart.RANDOM_TIME - Chart.PAUSE_TIME)
-                .attr('y', 180 - height - 1)
-                .attr('height', height);
-        })
-        this.arrow?.transition()
-            .duration(Chart.RANDOM_TIME - Chart.PAUSE_TIME)
-            .style('transform', `rotate(${Math.random() * 60 - 30}deg)`);
-    }
-}
+export class EmptyChart extends Chart { }
 
 export class PointChart extends Chart {
     points: Selection<SVGCircleElement, unknown, HTMLElement, undefined>[] = [];
@@ -188,13 +156,13 @@ export class HorizontalBarChart extends Chart {
                 .attr('y', 200 - y)
                 .attr('width', firstWidth)
                 .attr('height', height)
-                .style('fill', Chart.COLORS[i]);
+                .style('fill', Chart.COLORS.filter(e => e !== Chart.COLORS[2])[i]);
             const secondRect = this.svg.append('rect')
                 .attr('x', 21)
                 .attr('y', 200 - y - height - 2)
                 .attr('width', secondWidth)
                 .attr('height', height)
-                .style('fill', Chart.COLORS[i]);
+                .style('fill', Chart.COLORS.filter(e => e !== Chart.COLORS[2])[i]);
             this.bars.push(firstRect, secondRect);
         }
     }
@@ -274,6 +242,101 @@ export class LineChart extends Chart {
                     d += ` L${x} ${y}`;
                 }
             }
+            this.paths[colorIdx].transition()
+                .duration(Chart.RANDOM_TIME - Chart.PAUSE_TIME)
+                .attr('d', d);
+            for (let idx = 0; idx < points.length; idx++) {
+                this.dots[colorIdx][idx].transition()
+                    .duration(Chart.RANDOM_TIME - Chart.PAUSE_TIME)
+                    .attr('cx', points[idx].x)
+                    .attr('cy', points[idx].y);
+            }
+        }
+    }
+}
+
+export class StackAreaChart extends Chart {
+    paths: Selection<SVGPathElement, unknown, HTMLElement, undefined>[] = [];
+    dots: Selection<SVGCircleElement, unknown, HTMLElement, undefined>[][] = [];
+    create(selector: string): void {
+        const root = select(selector);
+        this.svg = root.append('svg')
+            .attr('width', 250)
+            .attr('height', 200);
+        const remainValues = Array(6).fill(160);
+        const usedValues = Array(6).fill(0);
+        for (let colorIdx = 0; colorIdx < Chart.COLORS.length; colorIdx++) {
+            const points: { x: number, y: number }[] = [];
+            let d = '';
+            for (let i = 0; i < 6; i++) {
+                const randomVal = Math.max(2, Math.min(30, Math.random() * remainValues[i]));
+                usedValues[i] += randomVal;
+                const y = 180 - usedValues[i];
+                remainValues[i] -= randomVal;
+                const x = 21 + i * 42;
+                points.push({ x, y });
+                if (i === 0) {
+                    d = `M${x} ${y}`;
+                } else {
+                    d += ` L${x} ${y}`;
+                }
+            }
+            d += ' L230 180 L20 180';
+            const path = this.svg.append('path')
+                .attr('d', d)
+                .style('fill', Chart.COLORS[colorIdx])
+                .style('stroke', Chart.COLORS[colorIdx])
+                .style('stroke-width', 1)
+                .lower();
+            this.paths.push(path);
+            const dots: Selection<SVGCircleElement, unknown, HTMLElement, undefined>[] = [];
+            for (let idx = 0; idx < points.length; idx++) {
+                const dot = this.svg.append('circle')
+                    .attr('cx', points[idx].x)
+                    .attr('cy', points[idx].y)
+                    .attr('r', 0)
+                    .style('fill', Chart.COLORS[colorIdx])
+                    .lower();
+                dots.push(dot);
+            }
+            this.dots.push(dots);
+        }
+        this.svg.append('path')
+            .attr('d', 'M20 20 L20 180 L230 180')
+            .style('fill', 'none')
+            .style('stroke', Chart.LEGEND_COLOR)
+            .style('stroke-width', 2)
+            .lower();
+        for (let i = 20; i < 180; i += 20) {
+            this.svg.append('path')
+                .attr('d', `M25 ${i} L230 ${i}`)
+                .style('fill', 'none')
+                .style('stroke', Chart.LEGEND_COLOR)
+                .style('stroke-width', 1)
+                .style('stroke-dasharray', '1')
+                .lower();
+        }
+    }
+    refresh(): void {
+        const remainValues = Array(6).fill(160);
+        const usedValues = Array(6).fill(0);
+        for (let colorIdx = 0; colorIdx < Chart.COLORS.length; colorIdx++) {
+            const points: { x: number, y: number }[] = [];
+            let d = '';
+            for (let i = 0; i < 6; i++) {
+                const randomVal = Math.max(2, Math.min(30, Math.random() * remainValues[i]));
+                usedValues[i] += randomVal;
+                const y = 180 - usedValues[i];
+                remainValues[i] -= randomVal;
+                const x = 21 + i * 42;
+                points.push({ x, y });
+                if (i === 0) {
+                    d = `M${x} ${y}`;
+                } else {
+                    d += ` L${x} ${y}`;
+                }
+            }
+            d += ' L230 180 L20 180';
             this.paths[colorIdx].transition()
                 .duration(Chart.RANDOM_TIME - Chart.PAUSE_TIME)
                 .attr('d', d);
